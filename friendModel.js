d324f0c95307bec3f19f38a9b7f007b2555ef674
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2020-08-15 13:09:27 -0500

    created friendModel

---
+++ friendModel.js
@@ -0,0 +1,36 @@
+// import mongoose to access MongoDB
+const mongoose = require('mongoose')
+
+// just a JS object to pre make the Schema
+const friendDefinition = {
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  age: {
+      type: Number,
+      min: 1,
+      max: 120,
+      required: true
+  },
+  createdOn: {
+      type: Date,
+      default: Date.now
+  }
+}
+
+// another JS object for options
+const options = {
+    timestamps: true
+}
+// instantiate mongoose schema & pass it the 2 objs made above
+const friendSchema = new mongoose.Schema(friendDefinition, options)
+
+// finally, pass the schema to mongoose here to make friendModel
+const friendModel = mongoose.model('Friend', friendSchema, 'friends')
+
+module.exports = friendModel
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 // https://www.tutorialspoint.com/mongodb/ -> online resource to learn mongoDB
 // https://mongoosejs.com/docs/ -> online resource to learn Mongoose ODM -> dafuq ODM stand for?
+// hmm...ORM stands for Object Relational Mapper, so does ODM stand for Object Dorkibus Mapper?
 
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+
+// ********* TODO: *********
+
+// 1) connect to MongoDB here:
+
+// 2) add friendController here:
 
 const server = express();
 
@@ -17,3 +24,11 @@ server.get('/', (req, res) => {
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
+
+
+// ************************ TODO: ************************
+// -> Review MongoDB shell commands
+// -> MongoDB admin? Wrap up any loose ends
+// -> Review Mongoose
+// -> Review helmet -> other than middleware, what is it?
+// -> Review cors -> other than middleware, what is it?
